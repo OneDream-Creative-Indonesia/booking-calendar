e37448fb1ee35305dcc94144eb360464ea9cf134
dedenkoesuma <dedenkoesuma72@gmail.com>
2025-07-20 19:44:34 +0700

    add favicon

--- app/Filament/Resources/BackgroundResource.php
+++ app/Filament/Resources/BackgroundResource.php
@@ -61,6 +61,7 @@ class BackgroundResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
